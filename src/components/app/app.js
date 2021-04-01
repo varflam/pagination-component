081e0a62cc63db6072ffef5ded3e5cfb47f82f48
Maria <alece.1414@gmail.com>
2021-04-01 17:47:35 +0300

    final changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import Card from '../cards/cards';
+import React, {useState} from 'react';
 import Pagination from '../Pagination/pagination';
 
 import './app.css';
 
 const App = () => {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setPage] = useState(1);
+    const [page, setPage] = useState(1);
     const [perPage, setPerPage] = useState(10);
 
     const getPage = (page) => {
@@ -19,27 +15,8 @@ const App = () => {
         setPerPage(perPage);
     }
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            setLoading(true);
-            const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-            setPosts(res.data);
-            setLoading(false);
-        }
-
-        fetchPosts();
-    }, []);
-
-    const indexOfLastPost = currentPage * perPage;
-    const indexOfFirstPost = indexOfLastPost - perPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
     return(
-        <div className="container">
-            <h1 className="text-primary">Anime titles</h1>
-            <Card posts={currentPosts} loading={loading}/>
-            <Pagination totalCount={posts.length} getPage={getPage} getPerPage={getPerPage}/>
-        </div>
+        <Pagination totalCount={100} getPage={getPage} getPerPage={getPerPage}/>
     )
 }
 
